7c6fe536dda1f4d28a47d0bde2221fb73c33a2b2
HAlipour8463 <h.alipour@unimelb.edu.au>
2020-08-26 16:05:22 +1000

    adopted for any metadata with different number of features or algorithms

--- purifyInstIS_Cmp.m
+++ purifyInstIS_Cmp.m
@@ -14,7 +14,7 @@ function purifyInstIS_Cmp(Net, epsilon, flag)
 % Net: is the name of metadata file including the instances and their features
 % and APs.
 % epsilon: determines the treshhold of the similarity.
-% flag: determines which similarity approach must be applied as follows; 
+% flag: determines which similarity approach must be applied as follows;
 % =========================================================================
 % 'Ftr': similarity just based on the features
 % 'Ftr&AP': both features and APs with Euclidian distance
@@ -32,9 +32,9 @@ function purifyInstIS_Cmp(Net, epsilon, flag)
 %          School of Mathematics and Statistics
 %          The University of Melbourne
 %          Australia
-%          2021
+%          2020
 %          Email: h.alipour@unimelb.edu.au
- 
+
 %  Copyright: Hossein Alipour
 %
 
@@ -54,6 +54,8 @@ for k = 1:length(epsilon)
     varlabels = Xbar.Properties.VariableNames;
     isfeat = strncmpi(varlabels,'feature_',8);
     isalgo = strncmpi(varlabels,'algo_',5);
+    numFtr = sum(isfeat);
+    numAlgo = sum(isalgo);
     X = Xbar{:,isfeat};
     Y = Xbar{:,isalgo};
     
@@ -71,38 +73,37 @@ for k = 1:length(epsilon)
     ViC_Good = 0; % A counter of the violation from the goodness criterion for APs
     %% Parameters for purifying APs based on the goodnes criterion
     
-%    flag
-        if (strcmp(flag, 'Ftr&Good') || strcmp(flag, 'Ftr&AP&Good'))
-            
-            opts.perf.MaxPerf = false;              % True if Y is a performance measure to maximize, False if it is a cost measure to minimise.
-            opts.perf.AbsPerf = false;               % True if an absolute performance measure, False if a relative performance measure
-            opts.perf.epsilon = 0.05;               % Threshold of good performance
-            
-            % -------------------------------------------------------------------------
-            if opts.perf.MaxPerf
-                Yaux = Y;
-                Yaux(isnan(Yaux)) = -Inf;
-                [rankPerf,rankAlgo] = sort(Yaux,2,'descend');
-                bestPerformace = rankPerf(:,1);
-                P = rankAlgo(:,1);
-                if opts.perf.AbsPerf
-                    Ybin = Yaux>=opts.perf.epsilon;
-                else
-                    Ybin = bsxfun(@ge,Yaux,(1-opts.perf.epsilon).*bestPerformace); % One is good, zero is bad
-                end
+    %    flag
+    if (strcmp(flag, 'Ftr&Good') || strcmp(flag, 'Ftr&AP&Good'))
+        
+        opts.perf.MaxPerf = false;              % True if Y is a performance measure to maximize, False if it is a cost measure to minimise.
+        opts.perf.AbsPerf = false;               % True if an absolute performance measure, False if a relative performance measure
+        opts.perf.epsilon = 0.05;               % Threshold of good performance
+        
+        % -------------------------------------------------------------------------
+        if opts.perf.MaxPerf
+            Yaux = Y;
+            Yaux(isnan(Yaux)) = -Inf;
+            [rankPerf,rankAlgo] = sort(Yaux,2,'descend');
+            bestPerformace = rankPerf(:,1);
+            P = rankAlgo(:,1);
+            if opts.perf.AbsPerf
+                Ybin = Yaux>=opts.perf.epsilon;
             else
-                Yaux = Y;
-                Yaux(isnan(Yaux)) = Inf;
-                [rankPerf,rankAlgo] = sort(Yaux,2,'ascend');
-                bestPerformace = rankPerf(:,1);
-                P = rankAlgo(:,1);
-                if opts.perf.AbsPerf
-                    Ybin = Yaux<=opts.perf.epsilon;
-                    msg = [msg 'less than ' num2str(opts.perf.epsilon)];
-                else
-                    Ybin = bsxfun(@le,Yaux,(1+opts.perf.epsilon).*bestPerformace);
-                end
+                Ybin = bsxfun(@ge,Yaux,(1-opts.perf.epsilon).*bestPerformace); % One is good, zero is bad
             end
+        else
+            Yaux = Y;
+            Yaux(isnan(Yaux)) = Inf;
+            [rankPerf,rankAlgo] = sort(Yaux,2,'ascend');
+            bestPerformace = rankPerf(:,1);
+            P = rankAlgo(:,1);
+            if opts.perf.AbsPerf
+                Ybin = Yaux<=opts.perf.epsilon;
+            else
+                Ybin = bsxfun(@le,Yaux,(1+opts.perf.epsilon).*bestPerformace);
+            end
+        end
     end
     
     
@@ -122,7 +123,7 @@ for k = 1:length(epsilon)
                                 Tprcl = Tprcl +1;
                             case 'Ftr&AP'
                                 dist_AP = sqrt(sum((Y(i,:) - Y(j,:)) .^ 2));
-                                if (dist_AP <= sqrt(4/17)*epsilon(k)*(1+0.26)) % 0.26 is the value of CPUN
+                                if (dist_AP <= sqrt(numAlgo/numFtr)*epsilon(k)*(1+0.26)) % 0.26 is the value of CPUN (CPU Noise)
                                     toPreclude(j) = true;
                                     Tprcl = Tprcl +1;
                                 else
@@ -138,53 +139,55 @@ for k = 1:length(epsilon)
                             case 'Ftr&AP&Good'
                                 if (Ybin(i,:) == Ybin(j,:))
                                     dist_AP = sqrt(sum((Y(i,:) - Y(j,:)) .^ 2));
-                                    if (dist_AP <= sqrt(4/17)*epsilon(k)*(1+0.26)) % epsilon could be different from that given in the function.
+                                    if (dist_AP <= sqrt(numAlgo/numFtr)*epsilon(k)*(1+0.26)) % epsilon could be different from that given in the function.
                                         toPreclude(j) = true;
                                         Tprcl = Tprcl +1;
                                     else
                                         ViC_D = ViC_D + 1;
                                     end
                                 else
-                                        ViC_Good  = ViC_Good + 1;
+                                    ViC_Good  = ViC_Good + 1;
                                 end
-                            end
-                        end
+                            otherwise
+                                disp('Invalid flag!')
+                         end
                     end
                 end
             end
-    end
-        
-    Tprcl; 
-    ViC_D ;
-    ViC_Good; 
-        
-        
-        %  Xbar = sortrows(Xbar); % This command is useful if you want to apply a random purification.
-        
-        PurifiedInst = Xbar;
-        PurifiedInst(toPreclude,:) = [];
-        PrecludedInst = Xbar;
-        PrecludedInst(~toPreclude,:) = [];
-        
-        switch flag
-            case 'Ftr'
-                writetable(PurifiedInst,sprintf('Purified2_%s_%s_Dist_%.3f.csv',flag, Net, epsilon(k)));
-                writetable(PrecludedInst,sprintf('Precluded2_%s_%s_Dist_%.3f.csv',flag, Net, epsilon(k)));
-            case 'Ftr&AP'
-                writetable(PurifiedInst,sprintf('Purified2_%s_%s_Dist_%.3f.csv',flag, Net, epsilon(k)));
-                writetable(PrecludedInst,sprintf('Precluded2_%s_%s_Dist_%.3f.csv',flag, Net, epsilon(k)));
-            case 'Ftr&Good'
-                writetable(PurifiedInst,sprintf('Purified2_%s_%s_G_%.2f_Dist_%.3f.csv',flag, Net,  opts.perf.epsilon, epsilon(k)));
-                writetable(PrecludedInst,sprintf('Precluded2_%s_%s_G_%.2f_Dist_%.3f.csv',flag, Net,  opts.perf.epsilon, epsilon(k)));
-            case 'Ftr&AP&Good'
-                writetable(PurifiedInst,sprintf('Purified2_%s_%s_G_%.2f_Dist_%.3f.csv',flag, Net,  opts.perf.epsilon, epsilon(k)));
-                writetable(PrecludedInst,sprintf('Precluded2_%s_%s_G_%.2f_Dist_%.3f.csv',flag, Net,  opts.perf.epsilon, epsilon(k)));
         end
-        %% Activate the following lines if there is more than one network and you want to
-        % collect all of them in a Purified_metadata file, which created by the main script, ISA.m;
-        %  Activvate the relevant commands in ISA.m is you want to create these metada files.
-        
-        %{
+    end
+    
+    Tprcl;
+    ViC_D ;
+    ViC_Good;
+    
+    
+    %  Xbar = sortrows(Xbar); % This command is useful if you want to apply a random purification.
+    
+    PurifiedInst = Xbar;
+    PurifiedInst(toPreclude,:) = [];
+    PrecludedInst = Xbar;
+    PrecludedInst(~toPreclude,:) = [];
+    
+    switch flag
+        case 'Ftr'
+            writetable(PurifiedInst,sprintf('Purified2_%s_%s_Dist_%.3f.csv',flag, Net, epsilon(k)));
+            writetable(PrecludedInst,sprintf('Precluded2_%s_%s_Dist_%.3f.csv',flag, Net, epsilon(k)));
+        case 'Ftr&AP'
+            writetable(PurifiedInst,sprintf('Purified2_%s_%s_Dist_%.3f.csv',flag, Net, epsilon(k)));
+            writetable(PrecludedInst,sprintf('Precluded2_%s_%s_Dist_%.3f.csv',flag, Net, epsilon(k)));
+        case 'Ftr&Good'
+            writetable(PurifiedInst,sprintf('Purified2_%s_%s_G_%.2f_Dist_%.3f.csv',flag, Net,  opts.perf.epsilon, epsilon(k)));
+            writetable(PrecludedInst,sprintf('Precluded2_%s_%s_G_%.2f_Dist_%.3f.csv',flag, Net,  opts.perf.epsilon, epsilon(k)));
+        case 'Ftr&AP&Good'
+            writetable(PurifiedInst,sprintf('Purified2_%s_%s_G_%.2f_Dist_%.3f.csv',flag, Net,  opts.perf.epsilon, epsilon(k)));
+            writetable(PrecludedInst,sprintf('Precluded2_%s_%s_G_%.2f_Dist_%.3f.csv',flag, Net,  opts.perf.epsilon, epsilon(k)));
+    end
+    %% Activate the following lines if there is more than one network and you want to
+    % collect all of them in a Purified_metadata file, which created by the main script, ISA.m;
+    %  Activvate the relevant commands in ISA.m is you want to create these metada files.
+    
+    %{
 PurifiedMainTbl = readtable(sprintf('Purified_metadata_Dist_%.2f.csv', epsilon));
 PurifiedMainTbl=[PurifiedMainTbl; PurifiedInst];
 writetable(PurifiedMainTbl, sprintf('Purified_metadata_Dist_%.2f.csv', epsilon));
@@ -192,13 +195,13 @@ writetable(PurifiedMainTbl, sprintf('Purified_metadata_Dist_%.2f.csv', epsilon))
 PrecludedMainTbl = readtable(sprintf('Precluded_metadata_Dist_%.2f.csv', epsilon));
 PrecludedMainTbl=[PrecludedMainTbl; PrecludedInst];
 writetable(PrecludedMainTbl, sprintf('Precluded_metadata_Dist_%.2f.csv', epsilon));
-        %}
-        %%
-        clear Xbar;
-        clear X;
-        clear Y;
-        clear Ybin;
-        clear toPreclude;
-        fclose('all');
-    end
-    
+    %}
+    %%
+    clear Xbar;
+    clear X;
+    clear Y;
+    clear Ybin;
+    clear toPreclude;
+    fclose('all');
+end
+
